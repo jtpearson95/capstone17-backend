1c49532b2ab9e81fd96d78d1ba326e67cd810d74
jtpearson95 <130475233+jtpearson95@users.noreply.github.com>
2024-01-19 12:12:03 -0500

    plugin

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({        
+    graphql: {
+      enabled: true,
+      config: {
+        endpoint: "/graphql",
+        shadowCRUD: true,
+        playgroundAlways: true,
+        defaultLimit: 10,
+        maxLimit: 20,
+        apolloServer: {
+          tracing: true,
+        },
+      },
+    },
+  });
