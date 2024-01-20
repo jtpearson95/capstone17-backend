cd3453b9709e831cec628c31f303fc3463313d1d
jtpearson95 <130475233+jtpearson95@users.noreply.github.com>
2024-01-20 10:29:03 -0500

    plugins introspection

--- config/plugins.js
+++ config/plugins.js
@@ -20,11 +20,12 @@
       config: {
         endpoint: '/graphql',
         shadowCRUD: true,
-        playgroundAlways: false,
+        playgroundAlways: true,
         depthLimit: 7,
         amountLimit: 100,
         apolloServer: {
           tracing: false,
+          introspection: true,
         },
       },
     },
